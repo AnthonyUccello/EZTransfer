6811f5f5a71a24871e48e4e5cef38206f3a19adf
Anthony Uccello <anthonyuccello@gmail.com>
2019-05-13 15:10:00 -0400

    Removed boilerplate

---
+++ EZTransfer/EZTransfer/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.swift
+//  EZTransfer
+//
+//  Created by Anthony Uccello on 2019-05-13.
+//  Copyright © 2019 Avanade. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+
+    }
+
+}
+
